b578e89e6524e4042d3540fd6c0233656b82e83d
Eugene Koontz <ekoontz@hiro-tan.org>
2011-02-20 20:06:45 -0800

    cleanup

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [slice "0.4.0-SNAPSHOT"]
                  [org.apache.lucene/lucene-core "3.0.2"]
                  [org.clojure/clojure "1.2.0"]
