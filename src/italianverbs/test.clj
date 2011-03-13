be53df8633830e6f3c43e1b7b05082589282e9e3
Eugene Koontz <ekoontz@hiro-tan.org>
2011-03-12 17:48:29 -0800

    working on conjugation tables.

--- src/italianverbs/test.clj
+++ src/italianverbs/test.clj
@@ -68,15 +68,16 @@
 		      (fs lexeme))
 		    (fetch :lexicon))))
 
-(defn io-andare []
-  (let [subject (pos (get-from-lexicon "io") 0 1)
+(defn conjugate-andare [pronoun]
+  (let [subject (pos (get-from-lexicon pronoun) 0 1)
 	verb-phrase (pos (get-from-lexicon "andare") 1 2)]
     (combine verb-phrase subject)))
 
+(defn io-andare []
+  (conjugate-andare "io"))
+
 (defn tu-andare []
-  (let [subject (pos (get-from-lexicon "tu") 0 1)
-	verb-phrase (pos (get-from-lexicon "andare") 1 2)]
-    (combine verb-phrase subject)))
+  (conjugate-andare "tu"))
 
 (defn io-pranzare []
   (let [subject (pos (get-from-lexicon "io") 0 1)
@@ -118,14 +119,14 @@
 
 (defn generate-np [offset]
   (let [noun
-	(positionalize 
+	(pos 
 	 (choose-lexeme
 	  (assoc {} :cat :noun))
 	  (+ offset 1)
 	  (+ offset 2))]
     ;; choose a determiner that agrees with the noun in number and gender.
     (let [determiner
-	  (positionalize
+	  (pos
 	   (choose-lexeme
 	    (assoc {}
 	      :gender (get noun :gender)
@@ -141,7 +142,7 @@
 		 :infl :infinitive
 		 :fn "trans-vo"}
 	verb
-	(positionalize
+	(pos
 	 (nth (fetch :lexicon :where verb-fs)
 	      (rand-int (count (fetch :lexicon :where verb-fs))))
 	 offset
@@ -153,7 +154,7 @@
  
 (defn generate-sentence []
   (let [subject
-	(positionalize
+	(pos
 	 (nth (fetch :lexicon :where {:cat :pronoun})
 	      (rand-int (count (fetch :lexicon :where {:cat :pronoun}))))
 	 0 1)] ;; fixme: left is always 0, but right (1)
@@ -166,21 +167,74 @@
 ;; current thing I'm debugging..
 (defn bugs []
    "<div> <h2>bugs</h2></div>"
-   (let [subject (positionalize (get-from-lexicon "lui") 0 1)
+   (let [subject (pos (get-from-lexicon "lui") 0 1)
 	 object (combine
-		 (positionalize (get-from-lexicon "donna") 3 4)
-		 (positionalize (get-from-lexicon "la") 2 3))
+		 (pos (get-from-lexicon "donna") 3 4)
+		 (pos (get-from-lexicon "la") 2 3))
 	 verb-phrase (combine
-		      (positionalize (get-from-lexicon "correggere") 1 2)
+		      (pos (get-from-lexicon "correggere") 1 2)
 		      object)]
 ;     (tablize verb-phrase)))
      (tablize (combine verb-phrase subject))))
+
+(defn conjugation [infinitive]
+  (let [fs (get-from-lexicon infinitive)]
+    (str
+     "<div class='conjugation'>"
+     (tablize fs)
+     "<table class='fs conjugation'>"
+       "<tr>"
+         "<th>io</th>"
+	 "<td>"
+	 (tablize (conjugate-andare "io"))
+	;	 (get (conjugate-andare "io") :italian)
+	 "</td>"
+	 "</tr>"
+       "<tr>"
+       "<th>tu</th>"
+       	 "<td>"
+	 (get (conjugate-andare "tu") :italian)
+	 "</td>"
+        "</tr>"
+       "<tr>"
+       "<th>lui/lei</th>"
+       	 "<td>"
+	 (get (conjugate-andare "lui") :italian)
+	 "</td>"
+
+        "</tr>"
+       "<tr>"
+       "<th>noi</th>"
+       	 "<td>"
+	 (get (conjugate-andare "noi") :italian)
+	 "</td>"
+
+        "</tr>"
+       "<tr>"
+       "<th>voi</th>"
+       	 "<td>"
+	 (get (conjugate-andare "voi") :italian)
+	 "</td>"
+
+        "</tr>"
+       "<tr>"
+       "<th>loro</th>"
+       	 "<td>"
+	 (get (conjugate-andare "loro") :italian)
+	 "</td>"
+
+        "</tr>"
+     "</table>"
+     "</div>"
+       )))
 
 (def tests
   (list
 ;   (reload-button) ; reload button does not work yet (results are still cached)
 
   ; (bugs)
+   "<div> <h2>conjugations</h2></div>"
+   (conjugation "andare")
    "<div> <h2>random sentences</h2></div>"
    (tablize (generate-sentence))
    (tablize (generate-sentence))
